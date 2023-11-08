a5e6f0899e6550a44747263d6b31856a9b409a54
Dark Fated <49955245+darkfated@users.noreply.github.com>
2023-11-08 12:49:30 +0300

    Refactoring

--- lua/fated_gang/data.lua
+++ lua/fated_gang/data.lua
@@ -1,32 +1,39 @@
-FatedGang.data = FatedGang.data or {}
+local GANG_FILE = 'fated_gang.json'
 
-hook.Add('Initialize', 'FatedGang.RestoreData', function()
-    if !file.Exists('fated_gang.json', 'DATA') then
-        file.Write('fated_gang.json', '[]')
+local function ReadData()
+    local data = {}
+
+    if file.Exists(GANG_FILE, 'DATA') then
+        data = util.JSONToTable(file.Read(GANG_FILE, 'DATA')) or {}
     end
 
-    FatedGang.data = util.JSONToTable(file.Read('fated_gang.json', 'DATA'))
+    FatedGang.data = data
     FatedGang.data.invites = {}
-end)
+end
 
-hook.Add('ShutDown', 'FatedGang.SaveAllData', function()
-    file.Write('fated_gang.json', util.TableToJSON(FatedGang.data))
+local function SaveData()
+    file.Write(GANG_FILE, util.TableToJSON(FatedGang.data))
 
-    for k, pl in pairs(player.GetAll()) do
+    for _, pl in pairs(player.GetAll()) do
         if pl:GetGangId() != '0' then
             pl:SetPData('fated_gang_id', pl:GetGangId())
         end
     end
-end)
+end
 
-hook.Add('PlayerInitialSpawn', 'FatedGang.ResetPlayerData', function(pl)
+local function OnPlayerInitialSpawn(pl)
     pl:SetGangId(pl:GetPData('fated_gang_id'))
 
     net.Start('FatedGang-ToClient')
         net.WriteTable(FatedGang.data)
     net.Send(pl)
-end)
+end
 
-hook.Add('PlayerDisconnected', 'FatedGang.SavePlayerData', function(pl)
+local function OnPlayerDisconnected(pl)
     pl:SetPData('fated_gang_id', pl:GetGangId())
-end)
+end
+
+hook.Add('Initialize', 'FatedGang.RestoreData', ReadData)
+hook.Add('ShutDown', 'FatedGang.SaveAllData', SaveData)
+hook.Add('PlayerInitialSpawn', 'FatedGang.ResetPlayerData', OnPlayerInitialSpawn)
+hook.Add('PlayerDisconnected', 'FatedGang.SavePlayerData', OnPlayerDisconnected)
